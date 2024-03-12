f96416e1d8e5137321031d691645a53a1a6c73c6
FOteroRimini <facurimini@gmail.com>
2024-03-12 19:06:38 -0300

    fix

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/home/home";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer"
 import Categories from "./components/Categories/Categories";
-import Indumentaria from "./components/Indumentaria/Indumentaria";
+import Detail from "./components/Detail/detail";
 
 
 const App = () => {
@@ -21,8 +21,8 @@ const App = () => {
        <div className="mainContent"></div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/indumentaria" element={<Indumentaria/>} />
         <Route path="/category/:id" element={<Categories/>} />
+        <Route path="/details/:id" element={<Detail />} />
       </Routes>
       <Footer />
     </div>
