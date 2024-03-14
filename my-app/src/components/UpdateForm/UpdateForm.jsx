714aebd755f8228e6b2a6008d81ad9cce04d6be3
FOteroRimini <facurimini@gmail.com>
2024-03-14 17:04:23 -0300

    Update and Create Forms

--- my-app/src/components/UpdateForm/UpdateForm.jsx
+++ my-app/src/components/UpdateForm/UpdateForm.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import { useState } from "react";
 import { validateArticle } from "../../utils/articleValidations";
 import { useDispatch, useSelector } from "react-redux";
-import { createArticle } from "../../redux/actions/articlesActions";
+import {
+  getArticlesById,
+  updateArticle,
+} from "../../redux/actions/articlesActions";
+import { getAllCategories } from "../../redux/actions/categoriesActions";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import style from "./update.module.css";
+import loader from "./loader.gif";
 
-const ArticleForm = () => {
+const UpdateArticleForm = () => {
+  const { id } = useParams();
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categoriesReducer.categories);
-
+  const articleToUpdate = useSelector((state) => state.articlesReducer.detail);
+  const [isLoading, setIsLoading] = useState(true);
   const [article, setArticle] = useState({
     name: "",
     description: "",
@@ -23,166 +33,186 @@ const ArticleForm = () => {
     size: "",
     CategoryId: "",
   });
-
   const [errors, setErrors] = useState({});
 
+  useEffect(() => {
+    dispatch(getAllCategories());
+    dispatch(getArticlesById(id)).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (articleToUpdate) {
+      setArticle(articleToUpdate);
+    }
+  }, [articleToUpdate]);
+
   const handleChange = (e) => {
-    const newValue = isNaN(parseFloat(e.target.value))
-      ? e.target.value
-      : parseFloat(e.target.value); // Convertir a número si es posible
-    setArticle(
-      (prevArticle) => ({
-        ...prevArticle,
+    const newValue =
+      e.target.type === "number" ? parseFloat(e.target.value) : e.target.value;
+    setArticle((prevArticle) => ({
+      ...prevArticle,
+      [e.target.name]: newValue,
+    }));
+    setErrors(
+      validateArticle({
+        ...article,
         [e.target.name]: newValue,
-      }),
-      () => {
-        // Este código se ejecuta después de que el estado article se haya actualizado completamente
-        setErrors(
-          validateArticle({
-            ...article,
-            [e.target.name]: newValue,
-          })
-        );
-      }
+      })
     );
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(createArticle(article));
+    dispatch(updateArticle(article)).then(() => {
+        alert("Artículo actualizado con éxito")});
     setErrors(validateArticle(article));
-    setArticle({
-      name: "",
-      description: "",
-      price: 0,
-      stockXS: 0,
-      stockS: 0,
-      stockM: 0,
-      stockL: 0,
-      stockXL: 0,
-      stockXXL: 0,
-      image: "",
-      size: "",
-      CategoryId: "",
-    });
   };
 
+  if (isLoading) {
+    return (
+      <div className={style.loaderContainer}>
+        <img src={loader} alt="Loading..." className={style.loader} />
+      </div>
+    );
+  }
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <h1>Crear Artículo</h1>
+        <h1>Editar Artículo</h1>
       </div>
-      <div>
-        <label>Nombre</label>
+      <div className={style.form}>
+        <div >
+          <img src={loader} alt="lewe" className={style.lewe} />
+        </div>
+        <label className={style.label}>Nombre</label>
         <input
           type="text"
           name="name"
           value={article.name}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.name && <p>{errors.name}</p>}
 
-        <label>Descripción Breve</label>
+        <label className={style.label}>Descripción Breve</label>
         <input
           type="text"
           name="description"
           value={article.description}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.description && <p>{errors.description}</p>}
 
-        <label>Precio</label>
+        <label className={style.label}>Precio</label>
         <input
           type="number"
           name="price"
           value={article.price}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.price && <p>{errors.price}</p>}
 
-        <label>Imagen</label>
+        <label className={style.label}>Imagen</label>
         <input
           type="text"
           name="image"
           value={article.image}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.image && <p>{errors.image}</p>}
 
-        <label>Categoría</label>
+        <label className={style.label}>Categoría</label>
         <select
           value={article.CategoryId}
           onChange={handleChange}
           name="CategoryId"
+          className={style.select}
         >
           {categories.map((category) => (
-            <option key={category.id} value={category.id}>
+            <option
+              key={category.id}
+              value={category.id}
+              className={style.options}
+            >
               {category.name}
             </option>
           ))}
         </select>
         {errors.CategoryId && <p>{errors.CategoryId}</p>}
 
-        <h3>Stock:</h3>
+        <h3 className={style.stock}>Stock:</h3>
 
-        <label>XS</label>
+        <label className={style.label}>XS</label>
         <input
           type="number"
           name="stockXS"
           value={article.stockXS}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.stockXS && <p>{errors.stockXS}</p>}
 
-        <label>S</label>
+        <label className={style.label}>S</label>
         <input
           type="number"
           name="stockS"
           value={article.stockS}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.stockS && <p>{errors.stockS}</p>}
 
-        <label>M</label>
+        <label className={style.label}>M</label>
         <input
           type="number"
           name="stockM"
           value={article.stockM}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.stockM && <p>{errors.stockM}</p>}
 
-        <label>L</label>
+        <label className={style.label}>L</label>
         <input
           type="number"
           name="stockL"
           value={article.stockL}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.stockL && <p>{errors.stockL}</p>}
 
-        <label>XL</label>
+        <label className={style.label}>XL</label>
         <input
           type="number"
           name="stockXL"
           value={article.stockXL}
           onChange={handleChange}
+          className={style.input}
         />
-        {errors.stockXl && <p>{errors.stockXL}</p>}
+        {errors.stockXL && <p>{errors.stockXL}</p>}
 
-        <label>XXL</label>
+        <label className={style.label}>XXL</label>
         <input
           type="number"
           name="stockXXL"
           value={article.stockXXL}
           onChange={handleChange}
+          className={style.input}
         />
         {errors.stockXXL && <p>{errors.stockXXL}</p>}
 
-        <button type="submit">Crear</button>
+        <button type="submit" className={style.create}>
+          Confirmar
+        </button>
       </div>
     </form>
   );
 };
 
-export default ArticleForm;
+export default UpdateArticleForm;
--- my-app/src/redux/actions/articlesActions.js
+++ my-app/src/redux/actions/articlesActions.js
@@ -3,6 +3,7 @@ import {
   GET_ARTICLES_BY_NAME,
   GET_ARTICLES_BY_ID,
   CREATE_ARTICLE,
+  UPDATE_ARTICLE,
 } from "../action-types/articlesContstants";
 import axios from "axios";
 
@@ -85,7 +86,6 @@ export const createArticle = (article) => {
         size,
         CategoryId,
       };
-      console.log(structuredArticle);
       const { data } = await axios.post(`/product`, structuredArticle);
       return dispatch({
         type: CREATE_ARTICLE,
@@ -96,3 +96,54 @@ export const createArticle = (article) => {
     }
   };
 };
+export const updateArticle = (article) => {
+  return async (dispatch) => {
+    try {
+      let {
+        id,
+        name,
+        description,
+        price,
+        stockXS,
+        stockS,
+        stockM,
+        stockL,
+        stockXL,
+        stockXXL,
+        image,
+        size,
+        CategoryId,
+      } = article;
+      price = price === "" ? 0 : parseFloat(price);
+      stockXS = stockXS === "" ? 0 : parseFloat(stockXS);
+      stockS = stockS === "" ? 0 : parseFloat(stockS);
+      stockM = stockM === "" ? 0 : parseFloat(stockM);
+      stockL = stockL === "" ? 0 : parseFloat(stockL);
+      stockXL = stockXL === "" ? 0 : parseFloat(stockXL);
+      stockXXL = stockXXL === "" ? 0 : parseFloat(stockXXL);
+      const structuredArticle = {
+        id,
+        name,
+        description,
+        price,
+        stockXS,
+        stockS,
+        stockM,
+        stockL,
+        stockXL,
+        stockXXL,
+        image,
+        size,
+        CategoryId,
+      };
+      const { data } = await axios.put(`/product`, structuredArticle);
+      return dispatch({
+        type: UPDATE_ARTICLE,
+        payload: data,
+      });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+}
+
