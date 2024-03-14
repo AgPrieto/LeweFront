8b29dd2b2529c5a803bc868768dcab7a020894f9
FOteroRimini <facurimini@gmail.com>
2024-03-14 11:04:30 -0300

    Create Article Form

--- my-app/src/components/CustomerForm/CustomerForm.jsx
+++ my-app/src/components/CustomerForm/CustomerForm.jsx
@@ -39,7 +39,7 @@ order: customer,
 articles: [{
 	id:"854ab5c4-44e2-4401-899e-82f498fd021b",
 	name:"Remera Blanca",
-    image:"https://fcppxyi.stripocdn.email/content/guids/CABINET_c67048fd0acf81b47e18129166337c05/images/79021618299486570.png",
+  image:"https://fcppxyi.stripocdn.email/content/guids/CABINET_c67048fd0acf81b47e18129166337c05/images/79021618299486570.png",
 	quantity:2,
 	size:"XXL",
 	price:"2000"
