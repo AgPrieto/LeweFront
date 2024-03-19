70b959bd12f7ccaee65b5a54125afd3961bf249d
FOteroRimini <facurimini@gmail.com>
2024-03-19 17:27:38 -0300

    AdminOrders

--- my-app/src/redux/action-types/orderContstants.js
+++ my-app/src/redux/action-types/orderContstants.js
@@ -1,1 +1,2 @@
 export const SEND_ORDER = "SEND_ORDER";
+export const GET_ORDERS = "GET_ORDERS";
--- my-app/src/redux/actions/orderActions.js
+++ my-app/src/redux/actions/orderActions.js
@@ -1,4 +1,4 @@
-import { SEND_ORDER } from "../action-types/orderContstants";
+import { SEND_ORDER, GET_ORDERS } from "../action-types/orderContstants";
 import axios from "axios";
 
 export const sendOrder = (order) => {
@@ -8,6 +8,20 @@ export const sendOrder = (order) => {
         window.open(data, '_blank', 'noopener');
         return dispatch({
             type: SEND_ORDER,
+            payload: data,
+        });
+        } catch (error) {
+        throw new Error(error.message);
+        }
+    };
+};
+
+export const getOrders = () => {
+    return async (dispatch) => {
+        try {
+        const {data} = await axios.get("/order");
+        return dispatch({
+            type: GET_ORDERS,
             payload: data,
         });
         } catch (error) {
