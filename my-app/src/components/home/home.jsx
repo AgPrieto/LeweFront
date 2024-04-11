2b6fa69a1dd57aa89ec03906b7ef95b280dbb2be
Agustin Prieto <aguspv99@gmail.com>
2024-04-11 18:30:11 -0300

    categorys

--- my-app/src/components/home/home.jsx
+++ my-app/src/components/home/home.jsx
@@ -3,15 +3,18 @@ import React, { useEffect } from 'react'
 import Carrusel from '../Carrusel/Carrusel.jsx'
 import { Link } from 'react-router-dom'
 import styles from "./home.module.css"
-import raquetas from "./RAQUETAS.png"
-import paletas from "./paletas.png"
-import indumentaria from "./indumentaria.png"
+import raquetas from "./bola.jpeg"
+import paletas from "./RaquetasCat.png"
+import indumentaria from "./indu.png"
 import accesorios from "./accesorios.png"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import InfiniteSlider from '../InfiniteSlider/InfiniteSlider.jsx';
 import { useSelector } from "react-redux";
 import HowToBuyContainer from "../Franja/franja.jsx";
+
+
+
 
 const Home = () => {
   const cart = useSelector((state) => state.cartReducer.cart);
@@ -32,93 +35,99 @@ const Home = () => {
     .sort(() => 0.5 - Math.random())
     .slice(0, 4);
 
-  return (
-    <div>
+    return (
       <div>
-        <Carrusel />
-      </div>
-      <div>
-        <InfiniteSlider />
-      </div>
-      <div
-        className={styles.categorys}>
-        
-        <div className={`${styles.category1} hvr-hang`}>
-          <Link to={"/category/108312e1-bed1-4468-aaed-657307fb2267"}>
-            <img data-aos="fade-right" data-aos-duration="1000"
-              src={accesorios}
-              alt="Accesorios"
-              className={`${styles.imagenFiltrotabla}`}
-              
-            />
-          </Link>
-        </div>
-        <div className={`${styles.category} hvr-hang`}>
-          {
-          <Link to={"/category/d5033fd4-8d56-4e02-b816-78b4f65ee660"}>
-            <img data-aos="fade-right" data-aos-duration="1000"
-              src={raquetas}
-              alt="Pelotas"
-              className={`${styles.imagenFiltrotabla}`}
-                         
-            />
-          </Link>
-          }
-        </div>
-        </div>
-        <div
-              className={styles.categorys}>
-        <div className={`${styles.category1} hvr-hang`}>
-          <Link to={"/category/4567773c-ab96-41aa-b9fa-ffa331fe4d7f"}>
-            <img data-aos="fade-right" data-aos-duration="1000"
-              src={paletas}
-              alt="Raquetas"
-              className={`${styles.imagenFiltrotabla}`}
-            />
-          </Link>
-        </div>
-        <div className={`${styles.category} hvr-hang`}>
-          <Link to={"/category/indumentaria"}>
-            <img data-aos="fade-right" data-aos-duration="1000"
-              src={indumentaria}
-              alt="Indumentaria"
-              className={`${styles.imagenFiltrotabla}`}
-        
-            />
-          </Link>
-        </div>
+        <div>
+          <Carrusel />
         </div>
         <div>
-        <HowToBuyContainer data-aos="fade-down"/>
+          <InfiniteSlider />
+        </div>
+        {/* Contenedor para los cuadros superiores */}
+        <div className={styles.topBoxesContainer}>
+          <div className={styles.category1}>
+            <Link to={"/category/108312e1-bed1-4468-aaed-657307fb2267"}>
+              <img
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                src={accesorios}
+                alt="Accesorios"
+                className={`${styles.imagenFiltrotabla}`}
+              />
+              <div className={styles.category1Text}>Accesorios</div>
+            </Link>
+          </div>
+          <div className={styles.category}>
+            <Link to={"/category/indumentaria"}>
+              <img
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                src={indumentaria}
+                alt="Indumentaria"
+                className={`${styles.imagenFiltrotabla}`}
+              />
+              <div className={styles.categoryText}>Indumentaria</div>
+            </Link>
+          </div>  
+        </div>
+        {/* Contenedor para los cuadros inferiores */}
+        <div className={styles.bottomBoxesContainer}>
+          <div className={styles.category1}>
+            <Link to={"/category/4567773c-ab96-41aa-b9fa-ffa331fe4d7f"}>
+              <img
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                src={paletas}
+                alt="Raquetas"
+                className={`${styles.imagenFiltrotabla}`}
+              />
+              <div className={styles.category1Text}>Raquetas</div>
+            </Link>
+          </div>
+          <div className={styles.category}>
+            <Link to={"/category/d5033fd4-8d56-4e02-b816-78b4f65ee660"}>
+              <img
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                src={raquetas}
+                alt="Pelotas"
+                className={`${styles.imagenFiltrotabla}`}
+              />
+              <div className={styles.categoryText}>Pelotas</div>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <HowToBuyContainer data-aos="fade-down"/>
         </div>
         <div className={styles.recommendedProductsContainer}>
-            <h2>PRODUCTOS DESTACADOS</h2>
-            <ul className={styles.recommendedProductsList}>
-              {recommendedProducts.map((item) => (
-                <div data-aos="fade-up" key={item.id}>
-                  <li className={styles.recommendedProductItem}>
-                    <Link
-                      to={`/details/${item.id}`}
-                      className={styles.recommendedLink}
-                    >
-                      <img
-                        src={item.image}
-                        alt={item.name}
-                        className={styles.recommendedImage}
-                      />
-                      <h2 className={styles.recommendedName}>{item.name}</h2>
-                      <p className={styles.recommendedPrice}>
-                        Precio: ${item.price}
-                      </p>
-                    </Link>
-                  </li>
-                </div>
-              ))}
-            </ul>
-          </div>
-    </div>
-  )
-}
-
-export default Home 
+          <h2>PRODUCTOS DESTACADOS</h2>
+          <ul className={styles.recommendedProductsList}>
+            {recommendedProducts.map((item) => (
+              <div data-aos="fade-up" key={item.id}>
+                <li className={styles.recommendedProductItem}>
+                  <Link
+                    to={`/details/${item.id}`}
+                    className={styles.recommendedLink}
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className={styles.recommendedImage}
+                    />
+                    <h2 className={styles.recommendedName}>{item.name}</h2>
+                    <p className={styles.recommendedPrice}>
+                      Precio: ${item.price}
+                    </p>
+                  </Link>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;  
 
